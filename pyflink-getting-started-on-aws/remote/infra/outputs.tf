567e3e7c9579f9af9feb2c2377287ea33c005a61
Jaehyeon Kim <dottami@gmail.com>
2023-08-23 17:04:34 +1000

    keep only iam auth

--- pyflink-getting-started-on-aws/remote/infra/outputs.tf
+++ pyflink-getting-started-on-aws/remote/infra/outputs.tf
@@ -63,19 +63,9 @@ output "msk_arn" {
   value       = aws_msk_cluster.msk_data_cluster.arn
 }
 
-output "msk_bootstrap_brokers" {
-  description = "One or more DNS names (or IP addresses) and PLAINTEXT port pairs"
-  value       = aws_msk_cluster.msk_data_cluster.bootstrap_brokers
-}
-
 output "msk_bootstrap_brokers_sasl_iam" {
   description = "One or more DNS names (or IP addresses) and SASL IAM port pairs"
   value       = aws_msk_cluster.msk_data_cluster.bootstrap_brokers_sasl_iam
-}
-
-output "msk_bootstrap_brokers_tls" {
-  description = "One or more DNS names (or IP addresses) and TLS port pairs"
-  value       = aws_msk_cluster.msk_data_cluster.bootstrap_brokers_tls
 }
 
 # KDA
